e2b4f26d33f4d4911ca6507fb7a1a64e28c4083a
federico goncalvez <97002761+fedegonc@users.noreply.github.com>
2025-09-27 11:36:57 -0300

    refactor: simplify app structure by removing boilerplate code and styles

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,12 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+// src/main.tsx
+
+import React from 'react'; // 💡 NECESARIO para usar <React.StrictMode>
+import { createRoot } from 'react-dom/client';
+import App from './App.tsx';
+// import './index.css';  <--- 💡 Elimina esta línea ya que borraste index.css
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
+  <React.StrictMode> // <--- 💡 Usar <React.StrictMode>
     <App />
-  </StrictMode>,
-)
+  </React.StrictMode>,
+);
