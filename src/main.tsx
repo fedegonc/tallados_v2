3a649758b0ed8433a570cbc14e39f0820acb0d9e
federico goncalvez <97002761+fedegonc@users.noreply.github.com>
2025-09-27 12:35:15 -0300

    chore: rename index.css to styles.css and uncomment import

--- src/main.tsx
+++ src/main.tsx
@@ -3,10 +3,10 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
-// import './index.css';
+import './styles.css';
 
 createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode> 
+  </React.StrictMode>
 );
